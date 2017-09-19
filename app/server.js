472eb821e600a8f147f6c2b743baf25e03dba788
roy769 <roy@mortgallery.com>
2017-09-18 23:17:15 -0700

    server work

---
+++ app/server.js
@@ -0,0 +1,18 @@
+//Dependecies
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+//set up express app
+
+var app = express();
+var PORT = process.env.PORT;
+
+//set up the express app to handle data parsing
+
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:"application/vnd.api+json"}));
+
+//friend finder matches (DATA)
+//=======================================================
